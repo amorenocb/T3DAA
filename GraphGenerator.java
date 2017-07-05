fe3d7e2067ee2230258baeabcd4a692320ed70e0
Juan Andres Moreno <amorenocb@gmail.com>
2017-07-05 11:20:56 -0400

    Added comments.

--- GraphGenerator.java
+++ GraphGenerator.java
@@ -12,8 +12,28 @@ import java.util.ArrayList;
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
 
+/**
+ * This class is used for generating graph test instances. It includes static methods
+ * for generating, saving and loading graph to and from memory. Specifically from .txt files.
+ * The format generated is as follows:
+ *
+ * Number of nodes
+ * Node0 NodeJ
+ * Node0 NodeI
+ * Node1 NodeL
+ * Node1 NodeM
+ *
+ * Where the first line indicates that there is an edge between Node0 and NodeJ.
+ */
 public class GraphGenerator {
 
+    /**
+     * Generates a graph with an specified number of nodes and putting an edge between every pair
+     * of nodes with probability p.
+     * @param numberOfNodes : Number of nodes in the generated graph.
+     * @param p : Probability that any two given nodes have an edge between them.
+     * @return AdjacencyList representing the generated graph.
+     */
     public static AdjacencyList generate(int numberOfNodes, double p){
         AdjacencyList graph = new AdjacencyList(numberOfNodes);
         for (int i = 0; i < numberOfNodes; i++) {
@@ -28,6 +48,15 @@ public class GraphGenerator {
         return graph;
     }
 
+    /**
+     * Given a AdjacencyList representation of a graph and and output file name
+     * it saves the graph a to .txt file with format specified in the beggining of this class
+     * documentation.
+     *
+     * @param aGraph : AdjacencyList representing graph to be saved.
+     * @param outputFileName : Name of the .txt file where the graph will be stored.
+     * @throws IOException
+     */
     public static void saveGraph(AdjacencyList aGraph, String outputFileName) throws IOException{
         Path outputFilePath = Paths.get(outputFileName);
         StringBuilder line = new StringBuilder();
@@ -52,7 +81,13 @@ public class GraphGenerator {
         }
     }
 
-
+    /**
+     * Loads a graph from memory. It is important to note that the graph need to be in the
+     * corresponding format.
+     *
+     * @param inputFileName : Name of .txt file where the graph is stored.
+     * @return AdjacencyList representing the loaded graph.
+     */
     public static AdjacencyList loadGraph(String inputFileName){
         AdjacencyList aGraph = null;
 
